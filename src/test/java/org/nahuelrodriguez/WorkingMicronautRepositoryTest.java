198990f9ef2ca9ab8c237c5ea8f61d1d2a21927e
Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>
2024-05-18 15:22:44 -0300

    test: Switched to H2 to simplify app execution.
    Signed-off-by: Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>

--- src/test/java/org/nahuelrodriguez/WorkingMicronautRepositoryTest.java
+++ src/test/java/org/nahuelrodriguez/WorkingMicronautRepositoryTest.java
@@ -1,45 +1,21 @@
 package org.nahuelrodriguez;
 
-import io.micronaut.core.annotation.NonNull;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.test.annotation.Sql;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import io.micronaut.test.support.TestPropertyProvider;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-import java.util.Map;
 
-@Testcontainers(disabledWithoutDocker = true)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Sql("classpath:sqls/init.sql")
+@Property(name = "datasources.default.driver-class-name", value = "org.h2.Driver")
+@Property(name = "datasources.default.url", value = "jdbc:h2:mem:h2")
+@Property(name = "datasources.default.username", value = "sa")
 @MicronautTest
-class WorkingMicronautRepositoryTest implements TestPropertyProvider {
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            DockerImageName.parse("postgres:12.19-alpine3.19")
-    );
-
+class WorkingMicronautRepositoryTest {
     @Inject
     private WorkingMicronautRepository subject;
-
-    @Override
-    public @NonNull Map<String, String> getProperties() {
-        if (!postgres.isRunning()) {
-            postgres.start();
-        }
-        return Map.of(
-                "datasources.default.driver-class-name", "org.postgresql.Driver",
-                "datasources.default.url", postgres.getJdbcUrl(),
-                "datasources.default.username", postgres.getUsername(),
-                "datasources.default.password", postgres.getPassword()
-        );
-    }
 
     @Test
     @DisplayName("Test that, hopefully, proves that buggy @Transactional behavior can't be caught when running tests")
