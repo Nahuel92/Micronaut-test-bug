fdfc92166f541b16a4f342aaead88ca5904c893a
Nahuel Rodríguez <12597182+Nahuel92@users.noreply.github.com>
2024-05-22 12:54:23 -0300

    chore: Exposing runtime `@Transactional` misbehavior

--- src/main/java/org/nahuelrodriguez/WorkingMicronautRepository.java
+++ src/main/java/org/nahuelrodriguez/WorkingMicronautRepository.java
@@ -7,7 +7,7 @@ import io.micronaut.data.jdbc.runtime.JdbcOperations;
 import java.util.List;
 
 @Repository
-//@io.micronaut.transaction.annotation.Transactional  // Works during testing time but not when running in Production mode
+@io.micronaut.transaction.annotation.Transactional  // Works during testing time but not when running in Production mode
 public abstract class WorkingMicronautRepository {
     private final JdbcOperations jdbcOperations;
 
@@ -19,7 +19,7 @@ public abstract class WorkingMicronautRepository {
         return List.of(5, 10, 15);
     }
 
-    @io.micronaut.transaction.annotation.Transactional
+    //@io.micronaut.transaction.annotation.Transactional
     public List<MyEntity> buggedMethod() {
         return jdbcOperations.prepareStatement(
                 "SELECT id, name FROM mytable",
